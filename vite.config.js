96e406ab317fb043d8958081b0582f25504eb45c
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2026-04-10 13:51:43 -0400

    fix tests

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: { port: 3000, open: true },
   build: { sourcemap: true },
-  test: {},
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.js'],
+  },
 });
